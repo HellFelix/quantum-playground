417c1e24501c5677facf6ae790b0cb1c9d4226f5
Felix Hellborg <felix.hellborg.la@gmail.com>
2024-04-21 19:28:38 +0200

    Add toggle buttons to UI for real and imaginary charts. Add comments to one dim visuals

--- src/one_dim/visuals.rs
+++ src/one_dim/visuals.rs
@@ -14,15 +14,24 @@ use nalgebra::DVector;
 use super::{iteration::rk4_iter_dt, wave, DT};
 use crate::complex::Complex;
 
+// creates bevy application and initiates simulation for one dimension
 pub fn oneD() {
     App::new()
         .add_plugins((DefaultPlugins, FrameTimeDiagnosticsPlugin))
+        // setup data
         .add_systems(Startup, setup)
+        // draw wave every frame
         .add_systems(Update, draw_wave_function)
-        .add_systems(PostUpdate, (update_wave_function, update_speed))
+        // update parameters and options after each frame
+        .add_systems(
+            PostUpdate,
+            (update_wave_function, update_params, update_options),
+        )
         .run();
 }
 
+// struct contraining all of the information of the actual simulation
+// i.e. holds all the information about the wave packet.
 #[derive(Component)]
 struct Data {
     raw: DVector<Complex>,
@@ -39,11 +48,41 @@ struct FrameText;
 #[derive(Component)]
 struct SpeedText;
 
+// holds informaiton about the buttons and which charts should be active
+#[derive(Component)]
+struct ToggleButton {
+    variant: ToggleVariant,
+    active: bool,
+    color: BackgroundColor,
+}
+impl ToggleButton {
+    pub fn variant(&self) -> ToggleVariant {
+        self.variant
+    }
+    pub fn active(&self) -> bool {
+        self.active
+    }
+    pub fn toggle(&mut self) {
+        self.active = !self.active;
+    }
+    pub fn color(&self) -> BackgroundColor {
+        self.color
+    }
+}
+// enum used to differentiate between the toggle buttons used in ToggleButton
+#[derive(Clone, Copy)]
+enum ToggleVariant {
+    Real,
+    Imag,
+}
+
 fn setup(mut commands: Commands) {
+    // camera settings
     let mut camera = Camera2dBundle::default();
     camera.projection.scale = 0.01;
     commands.spawn(camera);
 
+    // initial wave packet
     let wave = wave();
     let x = DVector::from(wave.0.iter().map(|x| *x as f32).collect::<Vec<f32>>());
     let raw = wave.1.clone();
@@ -63,7 +102,7 @@ fn setup(mut commands: Commands) {
     };
     commands.spawn(data);
 
-    // text
+    // Very basic UI to show relevant information and act as a functional interface
     commands
         .spawn(NodeBundle {
             style: Style {
@@ -76,6 +115,7 @@ fn setup(mut commands: Commands) {
             ..default()
         })
         .with_children(|parent| {
+            // Timer
             parent.spawn((
                 TextBundle::from_section(
                     "Time:",
@@ -88,6 +128,7 @@ fn setup(mut commands: Commands) {
                 AccessibilityNode(NodeBuilder::new(Role::ListItem)),
             ));
 
+            // Frame rate viewer
             parent.spawn((
                 TextBundle::from_section(
                     "FPS:",
@@ -100,6 +141,7 @@ fn setup(mut commands: Commands) {
                 AccessibilityNode(NodeBuilder::new(Role::ListItem)),
             ));
 
+            // Speed viewer
             parent.spawn((
                 TextBundle::from_section(
                     "Speed: ",
@@ -111,36 +153,152 @@ fn setup(mut commands: Commands) {
                 SpeedText,
                 AccessibilityNode(NodeBuilder::new(Role::ListItem)),
             ));
+        })
+        .with_children(|parent| {
+            parent
+                .spawn(NodeBundle {
+                    style: Style {
+                        padding: UiRect {
+                            top: Val::Vh(5.),
+                            right: Val::Px(5.),
+                            ..default()
+                        },
+                        ..default()
+                    },
+                    ..default()
+                })
+                .with_children(|parent| {
+                    parent.spawn((
+                        TextBundle::from_section(
+                            "Options: ",
+                            TextStyle {
+                                font_size: 25.,
+                                ..default()
+                            },
+                        ),
+                        AccessibilityNode(NodeBuilder::new(Role::ListItem)),
+                    ));
+                });
+
+            // Show real chart button
+            parent
+                .spawn((
+                    ButtonBundle {
+                        style: Style {
+                            width: Val::Percent(65.),
+                            height: Val::Vw(3.),
+                            border: UiRect::all(Val::Px(5.0)),
+                            margin: UiRect::all(Val::Px(5.0)),
+                            // horizontally center child text
+                            justify_content: JustifyContent::Center,
+                            // vertically center child text
+                            align_items: AlignItems::Center,
+
+                            ..default()
+                        },
+                        border_color: Color::RED.into(),
+                        background_color: Color::RED.into(),
+                        ..default()
+                    },
+                    ToggleButton {
+                        variant: ToggleVariant::Real,
+                        active: true,
+                        color: Color::RED.into(),
+                    },
+                    AccessibilityNode(NodeBuilder::new(Role::ListItem)),
+                ))
+                .with_children(|parent| {
+                    parent.spawn(TextBundle::from_section(
+                        "Real",
+                        TextStyle {
+                            font_size: 25.,
+                            ..default()
+                        },
+                    ));
+                });
+
+            // Show imaginary chart button
+            parent
+                .spawn((
+                    ButtonBundle {
+                        style: Style {
+                            width: Val::Percent(65.),
+                            height: Val::Vw(3.),
+                            border: UiRect::all(Val::Px(5.0)),
+                            margin: UiRect::all(Val::Px(5.0)),
+                            // horizontally center child text
+                            justify_content: JustifyContent::Center,
+                            // vertically center child text
+                            align_items: AlignItems::Center,
+
+                            ..default()
+                        },
+                        border_color: Color::BLUE.into(),
+                        background_color: Color::BLUE.into(),
+                        ..default()
+                    },
+                    ToggleButton {
+                        variant: ToggleVariant::Imag,
+                        active: true,
+                        color: Color::BLUE.into(),
+                    },
+                    AccessibilityNode(NodeBuilder::new(Role::ListItem)),
+                ))
+                .with_children(|parent| {
+                    parent.spawn(TextBundle::from_section(
+                        "Imaginary",
+                        TextStyle {
+                            font_size: 25.,
+                            ..default()
+                        },
+                    ));
+                });
         });
 }
-fn draw_wave_function(mut gizmos: Gizmos, data: Query<&Data>) {
+fn draw_wave_function(
+    mut gizmos: Gizmos,
+    data: Query<&Data>,
+    toggle_buttons_query: Query<&ToggleButton, With<Button>>,
+) {
     let data = data.get_single().unwrap();
-    for i in 0..data.x.len() - 1 {
-        gizmos.line_2d(
-            Vec2 {
-                x: data.x[i],
-                y: data.raw[i].real() as f32,
-            },
-            Vec2 {
-                x: data.x[i + 1],
-                y: data.raw[i + 1].real() as f32,
-            },
-            Color::RED,
-        );
-    }
 
-    for i in 0..data.x.len() - 1 {
-        gizmos.line_2d(
-            Vec2 {
-                x: data.x[i],
-                y: data.raw[i].imag() as f32,
-            },
-            Vec2 {
-                x: data.x[i + 1],
-                y: data.raw[i + 1].imag() as f32,
-            },
-            Color::BLUE,
-        );
+    for button in toggle_buttons_query.iter() {
+        match button.variant() {
+            ToggleVariant::Real => {
+                if button.active() {
+                    for i in 0..data.x.len() - 1 {
+                        gizmos.line_2d(
+                            Vec2 {
+                                x: data.x[i],
+                                y: data.raw[i].real() as f32,
+                            },
+                            Vec2 {
+                                x: data.x[i + 1],
+                                y: data.raw[i + 1].real() as f32,
+                            },
+                            Color::RED,
+                        );
+                    }
+                }
+            }
+            ToggleVariant::Imag => {
+                if button.active() {
+                    for i in 0..data.x.len() - 1 {
+                        gizmos.line_2d(
+                            Vec2 {
+                                x: data.x[i],
+                                y: data.raw[i].imag() as f32,
+                            },
+                            Vec2 {
+                                x: data.x[i + 1],
+                                y: data.raw[i + 1].imag() as f32,
+                            },
+                            Color::BLUE,
+                        );
+                    }
+                }
+            }
+        }
     }
 
     for i in 0..data.x.len() - 1 {
@@ -158,17 +316,12 @@ fn draw_wave_function(mut gizmos: Gizmos, data: Query<&Data>) {
     }
 }
 
-fn update_wave_function(
-    mut data: Query<&mut Data>,
-    diagnostics: Res<DiagnosticsStore>,
-    mut text_set: ParamSet<(
-        Query<&mut Text, With<TimeText>>,
-        Query<&mut Text, With<FrameText>>,
-    )>,
-) {
+fn update_wave_function(mut data: Query<&mut Data>) {
     // iterate
     let mut data = data.get_single_mut().unwrap();
     let mut next = data.raw.clone();
+    // skips to the next time step i.e. data.speed
+    // each iteration is still calculated, but the ones in between are not shown
     for _ in 0..data.speed {
         next = rk4_iter_dt(&next);
     }
@@ -182,6 +335,19 @@ fn update_wave_function(
     data.raw = next;
     data.prob = next_prob;
     data.time_passed += DT * data.speed as f64;
+}
+
+fn update_params(
+    mut data: Query<&mut Data>,
+    mut key_evs: EventReader<KeyboardInput>,
+    diagnostics: Res<DiagnosticsStore>,
+    mut text_set: ParamSet<(
+        Query<&mut Text, With<TimeText>>,
+        Query<&mut Text, With<FrameText>>,
+        Query<&mut Text, With<SpeedText>>,
+    )>,
+) {
+    let mut data = data.get_single_mut().unwrap();
 
     // update timer
     for mut timer in &mut text_set.p0() {
@@ -197,14 +363,8 @@ fn update_wave_function(
             }
         }
     }
-}
 
-fn update_speed(
-    mut data: Query<&mut Data>,
-    mut key_evs: EventReader<KeyboardInput>,
-    mut speed_query: Query<&mut Text, With<SpeedText>>,
-) {
-    let mut data = data.get_single_mut().unwrap();
+    // update speed
     for e in key_evs.read() {
         if e.key_code == KeyCode::ArrowUp && data.speed < 200 {
             data.speed += 1;
@@ -213,7 +373,31 @@ fn update_speed(
         }
     }
 
-    for mut speed_text in &mut speed_query {
+    for mut speed_text in &mut text_set.p2() {
         speed_text.sections[0].value = format!("Speed: {}", data.speed);
+    }
+}
+
+fn update_options(
+    mut interaction_query: Query<
+        (&Interaction, &mut BackgroundColor, &mut ToggleButton),
+        (Changed<Interaction>, With<Button>),
+    >,
+) {
+    for (interaction, mut color, mut toggle_button) in &mut interaction_query {
+        match *interaction {
+            Interaction::Pressed => {
+                // change the state of the chart in question
+                toggle_button.toggle();
+
+                // change the colour of the button to match its state
+                if toggle_button.active() {
+                    *color = toggle_button.color();
+                } else {
+                    *color = Color::BLACK.into();
+                }
+            }
+            _ => {}
+        };
     }
 }
